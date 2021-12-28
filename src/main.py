822c3685b0473a3edaf40a1a8d80b0bafa28c825
Aimer Neige <aimer.neige.soft@gmail.com>
2021-12-28 17:14:46 +0800

    basic config

---
+++ src/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Aimer Neige
+# Mail: aimer.neige@aimerneige.com
+
+def main():
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
