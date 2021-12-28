4df0babadbe9e1a85a29b1cb3ebbc2a184c2a36c
Aimer Neige <aimer.neige.soft@gmail.com>
2021-12-28 17:16:39 +0800

    update license

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # Author: Aimer Neige
 # Mail: aimer.neige@aimerneige.com
+# LICENSE: AGPLv3 (https://www.gnu.org/licenses/agpl-3.0.txt)
 
 def main():
     print("Hello World")
